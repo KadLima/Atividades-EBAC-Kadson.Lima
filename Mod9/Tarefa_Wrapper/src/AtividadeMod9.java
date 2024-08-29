5ea574c2c29095b3b6a4faeadf8e757362d180eb
Kadson Guilherme <kadsonlima91@gmail.com>
2024-08-29 00:27:30 -0300

    Atividade Módulo 9

---
+++ Mod9/Tarefa_Wrapper/src/AtividadeMod9.java
@@ -0,0 +1,23 @@
+/**
+ * @author Kadson Lima
+ *
+ */
+
+public class AtividadeMod9 {
+
+    public static void main(String[] args) {
+        int num = 25;
+        Integer wrapperNum = Integer.valueOf(num);
+        System.out.println(wrapperNum);
+
+        //Outros Exemplos apenas para fixar
+
+        float num1 = 1.35f;
+        Float wrapperNum1 = Float.valueOf(num1);
+        System.out.println(wrapperNum1);
+
+        long num2 = Long.MAX_VALUE;
+        Long wrapperNum2 = num2;
+        System.out.println(wrapperNum2);
+    }
+}
